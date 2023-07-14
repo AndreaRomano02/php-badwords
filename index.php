334397725dfd760d94d6b72eb9d2a28c4305826a
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-14 14:21:43 +0200

    Add home page with the form

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-var_dump('prova');
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP || BadWords</title>
+</head>
+
+<body>
+    <form action="badwords.php" method="GET">
+        <h2>Scrivi qua la parola da censurare</h2>
+        <input type="text" name="censored_word" placeholder="Inserisci la parola da censurare" value="ciao">
+
+        <h2>Scrivi qua la tua frase</h2>
+        <textarea name="string" cols="30" rows="10" placeholder="Inserisci una frase..."></textarea>
+        <button>Censura</button>
+    </form>
+</body>
+
+</html>
